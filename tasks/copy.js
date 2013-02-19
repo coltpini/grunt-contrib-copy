0aad6b1d6d974d3d054ce05769223df3bcd0d8e0
Colt Pini <coltpini@gmail.com>
2013-02-19 15:55:05 -0700

    Updated to allow for coping only of newer files.
    Only uses file size for now, that may change to incorporate size and dates, when I can figure out how to deal with dates correctly.

--- tasks/copy.js
+++ tasks/copy.js
@@ -9,13 +9,14 @@
 
 module.exports = function(grunt) {
   'use strict';
-
   var path = require('path');
+  var fs = require('fs');
 
   grunt.registerMultiTask('copy', 'Copy files.', function() {
     var kindOf = grunt.util.kindOf;
+    var helpers = require('grunt-lib-contrib').init(grunt);
 
-    var options = this.options({
+    var options = helpers.options(this, {
       processContent: false,
       processContentExclude: []
     });
@@ -34,22 +35,49 @@ module.exports = function(grunt) {
       isExpandedPair = filePair.orig.expand || false;
 
       filePair.src.forEach(function(src) {
+
         if (detectDestType(filePair.dest) === 'directory') {
           dest = (isExpandedPair) ? filePair.dest : unixifyPath(path.join(filePair.dest, src));
         } else {
           dest = filePair.dest;
         }
-
-        if (grunt.file.isDir(src)) {
-          grunt.log.writeln('Creating ' + dest.cyan);
-          grunt.file.mkdir(dest);
-        } else {
-          grunt.log.writeln('Copying ' + src.cyan + ' -> ' + dest.cyan);
-          grunt.file.copy(src, dest, copyOptions);
+        // new stuff.
+        if(filePair.onlyNewer){
+          var srcFile, destFile;
+          try{
+            srcFile = fs.statSync(src);
+            destFile = fs.statSync(dest);
+          }
+          catch(err){
+            // no dest file.
+            srcFile = false;
+            destFile = false;
+          }
+          if(srcFile && destFile){
+            if(!grunt.file.isDir(src) && (srcFile.size !== destFile.size /*|| srcFile.mtime > destFile.ctime*/ )){
+                grunt.log.writeln('Copying ' + src.cyan + ' -> ' + dest.cyan);
+                grunt.file.copy(src, dest, copyOptions);
+            }
+          }
+          else {
+            createNew(grunt,src,dest,copyOptions);
+          }
+        }
+        else {
+          createNew(grunt,src,dest,copyOptions);
         }
       });
     });
   });
+  var createNew = function(grunt,src,dest,copyOptions){
+    if (grunt.file.isDir(src)) {
+      grunt.log.writeln('Creating ' + dest.cyan);
+      grunt.file.mkdir(dest);
+    } else {
+      grunt.log.writeln('Copying ' + src.cyan + ' -> ' + dest.cyan);
+      grunt.file.copy(src, dest, copyOptions);
+    }
+  };
 
   var detectDestType = function(dest) {
     if (grunt.util._.endsWith(dest, '/')) {
